7f58652424d5e9653a1f339f75b95eb07c41933c
Jemscape <jp3to8@hotmail.com>
2013-09-30 11:28:43 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Apprails::Application.config.secret_token = '3935fed63b5ce30de693fe7183aab91d50460fef71531e09db2cb1bfdcdb93d0d0b0ae5bbafd85969aabb65f94705da079e033a371e4cfe36ea55b547f9a22a0'
